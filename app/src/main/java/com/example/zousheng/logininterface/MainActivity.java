edbd9033ac10e68543349c4d8b61f1bfd21f0a0b
Zou Sheng <382223154@qq.com>
2015-12-04 17:16:37 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/zousheng/logininterface/MainActivity.java
+++ app/src/main/java/com/example/zousheng/logininterface/MainActivity.java
@@ -18,8 +18,6 @@ public class MainActivity extends  AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SharedPreferences sp = getSharedPreferences("sdls",MODE_PRIVATE);
-        SharedPreferences.Editor localsp=sp.edit();
         button=(Button)findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
